f1be4f2a6ce45e609b53261deb57bf1359a040ab
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-08 14:35:44 +0000

    fs

--- Day-11/crypto.js
+++ Day-11/crypto.js
@@ -1,6 +1,15 @@
 // crypto.randomBytes(size)
 const crypto = require("crypto")
 
-const randomValue = crypto.randomBytes(6).toString();
+const randomValue = crypto.randomBytes(6).toString("hex");
 
 console.log(randomValue)
+
+const genRandom = (digit)=>{
+    const start = digit ===6 ? 100000 : 100000;
+    const end = digit ===6 ? 999999 : 999999;
+    return crypto.randomInt(start, end)
+
+}
+
+console.log(genRandom(7))
