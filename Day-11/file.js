73464c5fd1677e8ab6405efb8e9bd093383df366
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-28 16:42:36 +0545

    fs in node js

---
+++ Day-11/file.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+
+fs.writeFileSync("./nishan.txt", "Hi Nishan Rai");
+
+//async 
+
+fs.writeFile("./nisha.txt", "hello nishan",(err)=>{
+
+})
+// sync 
+const  result = fs.readFileSync("./contact.txt", "utf-8");
+console.log(result)
+
+// Async 
+fs.readFile("./contact.txt", "utf-8",(err, result)=>{
+    if(err){
+        console.log(" Error ocure", err)
+    }else{
+        console.log(result)
+    }
+})
+
+
+fs.appendFileSync("./contact.txt","you are from sindhuli")
+
+fs.cpSync("./contact.txt","./copy.txt")
+console.log(fs.statSync("./copy.txt"))
